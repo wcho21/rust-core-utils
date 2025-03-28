451f7b7f1a31fa69b2a4efdecc69be7f5dae596d
Cho <wcho.dev@gmail.com>
2025-03-28 23:27:53 +0900

    refactor args parsing into function

--- echo_r/src/lib.rs
+++ echo_r/src/lib.rs
@@ -1,6 +1,6 @@
 const SEPARATOR: &str = " ";
 
-pub fn echo(text: Vec<&str>, omit_newline: bool) {
+pub fn echo(text: &Vec<String>, omit_newline: bool) {
     let ending = if omit_newline { "" } else { "\n" };
 
     print!("{}{}", text.join(SEPARATOR), ending);
--- echo_r/src/main.rs
+++ echo_r/src/main.rs
@@ -1,32 +1,36 @@
 use clap::{Arg, ArgAction, Command};
 
 fn main() {
-    // define args
-    let text_arg = Arg::new("text")
+    let (text, omit_newline) = get_args();
+
+    echo_r::echo(&text, omit_newline);
+}
+
+fn get_args() -> (Vec<String>, bool) {
+    let text = Arg::new("text")
         .value_name("TEXT")
         .help("Input text")
         .num_args(1..)
         .required(true);
 
-    let omit_newline_arg = Arg::new("omit_newline")
+    let omit_newline = Arg::new("omit_newline")
         .short('n')
         .help("Do not print the trailing newline character.")
         .num_args(0)
         .action(ArgAction::SetTrue);
 
-    // parse args
     let matches = Command::new("echo_r")
         .version("0.1.0")
-        .arg(text_arg)
-        .arg(omit_newline_arg)
+        .arg(text)
+        .arg(omit_newline)
         .get_matches();
 
-    let text_matched: Vec<&str> = matches
+    let text: Vec<String> = matches
         .get_many::<String>("text")
         .unwrap()
-        .map(String::as_str)
+        .cloned()
         .collect();
-    let omit_newline_matched = matches.get_flag("omit_newline");
+    let omit_newline = matches.get_flag("omit_newline");
 
-    echo_r::echo(text_matched, omit_newline_matched);
+    (text, omit_newline)
 }
